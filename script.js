9b4e4df20dabcf903c29a43a9f42422a74cde7db
Schlafschafe <dieschlafschafe@gmail.com>
2022-08-07 22:28:12 +0200

    feature: finalized css carousel
    removed unnessesary js code, removed style, removed onpress event

--- script.js
+++ script.js
@@ -7,55 +7,6 @@ const openNav = () => {
   document.getElementById("navigation").classList.toggle("visibility");
 };
 
-// method to change between different tiles for mobile
-/* function showSlides(n, kind) {
-  let i;
-  let slides = document.getElementsByClassName(`${kind}-slide`);
-  let dots = document.getElementsByClassName(`${kind}-dot`);
-  if (n > slides.length) {
-    slideIndex = 1;
-  }
-  if (n < 1) {
-    slideIndex = slides.length;
-  }
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex - 1].style.display = "flex";
-  dots[slideIndex - 1].className += " active";
-}
-// Next/previous controls
-function plusSlides(n) {
-  showSlides((slideIndex += n));
-}
-
-// Thumbnail image controls
-function currentSlide(n, kind) {
-  showSlides((slideIndex = n), kind);
-} */
-
-// method to detect small screen in js with media query
-/* const x = window.matchMedia("(max-width: 780px)");
-if (x.matches) {
-  showSlides(slideIndex, "project");
-  showSlides(slideIndex, "benefit");
-}
-x.onchange = (e) => {
-  if (e.matches) {
-    showSlides(slideIndex, "project");
-    showSlides(slideIndex, "benefit");
-  } else {
-    let slides = document.getElementsByClassName("slide");
-    for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "flex";
-    }
-  }
-};
- */
-
 const toggleDots = (n, kind) => {
   const dots = document.getElementsByClassName(`${kind}-dot`);
   for (i = 0; i < dots.length; i++) {
