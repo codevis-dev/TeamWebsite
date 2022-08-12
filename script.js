f3c28e136c21ab269b69ff2f43136af373dceb8c
maxbrng <90261666+maxbrng@users.noreply.github.com>
2022-08-12 21:30:58 +0200

    added hamburger animation

--- script.js
+++ script.js
@@ -219,3 +219,15 @@ const benefitCarousel = document.getElementById("benefit-carousel");
 
 projectCarousel.addEventListener("scroll", scrollHandler);
 benefitCarousel.addEventListener("scroll", scrollHandler);
+
+const menuBtn = document.querySelector(".menu-btn");
+let menuOpen = false;
+menuBtn.addEventListener("click", () => {
+  if (!menuOpen) {
+    menuBtn.classList.add("open");
+    menuOpen = true;
+  } else {
+    menuBtn.classList.remove("open");
+    menuOpen = false;
+  }
+});
